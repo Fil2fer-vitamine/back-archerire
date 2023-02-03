2eefbf363530edfb08a303cf7a727d215b445daa
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-03 22:10:31 +0100

    Injection du repository via TypeORM - Service

--- src/animationsrequested/animationsrequested.service.ts
+++ src/animationsrequested/animationsrequested.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateAnimationsrequestedDto } from './dto/create-animationsrequested.dto';
 import { UpdateAnimationsrequestedDto } from './dto/update-animationsrequested.dto';
+import { Animationsrequested } from './entities/animationsrequested.entity';
 
 @Injectable()
 export class AnimationsrequestedService {
+  constructor(
+    @InjectRepository(Animationsrequested)
+    private animationrequestedRepository: Repository<Animationsrequested>,
+  ) {}
+  
   create(createAnimationsrequestedDto: CreateAnimationsrequestedDto) {
     return 'This action adds a new animationsrequested';
   }
@@ -16,7 +24,10 @@ export class AnimationsrequestedService {
     return `This action returns a #${id} animationsrequested`;
   }
 
-  update(id: number, updateAnimationsrequestedDto: UpdateAnimationsrequestedDto) {
+  update(
+    id: number,
+    updateAnimationsrequestedDto: UpdateAnimationsrequestedDto,
+  ) {
     return `This action updates a #${id} animationsrequested`;
   }
 
--- src/loyaltycards/loyaltycards.service.ts
+++ src/loyaltycards/loyaltycards.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLoyaltycardDto } from './dto/create-loyaltycard.dto';
 import { UpdateLoyaltycardDto } from './dto/update-loyaltycard.dto';
+import { Loyaltycard } from './entities/loyaltycard.entity';
 
 @Injectable()
 export class LoyaltycardsService {
+  constructor(
+    @InjectRepository(Loyaltycard)
+    private loyaltycardRepository: Repository<Loyaltycard>,
+  ) {}
+  
   create(createLoyaltycardDto: CreateLoyaltycardDto) {
     return 'This action adds a new loyaltycard';
   }
