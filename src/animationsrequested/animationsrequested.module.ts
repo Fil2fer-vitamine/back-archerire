dc46c7f9daf7da493f569c4b0150cae0bfafed78
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-03 21:59:46 +0100

    Injection du repository  via TypeORM - Module

--- src/animationsrequested/animationsrequested.module.ts
+++ src/animationsrequested/animationsrequested.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnimationsrequestedService } from './animationsrequested.service';
 import { AnimationsrequestedController } from './animationsrequested.controller';
+import { Animationsrequested } from './entities/animationsrequested.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Animationsrequested])],
   controllers: [AnimationsrequestedController],
-  providers: [AnimationsrequestedService]
+  providers: [AnimationsrequestedService],
 })
 export class AnimationsrequestedModule {}
--- src/loyaltycards/loyaltycards.module.ts
+++ src/loyaltycards/loyaltycards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoyaltycardsService } from './loyaltycards.service';
 import { LoyaltycardsController } from './loyaltycards.controller';
+import { Loyaltycard } from './entities/loyaltycard.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Loyaltycard])],
   controllers: [LoyaltycardsController],
-  providers: [LoyaltycardsService]
+  providers: [LoyaltycardsService],
 })
 export class LoyaltycardsModule {}
