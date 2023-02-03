51e5419e7ab26aa750723cefb4e9ee49b3a0001f
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-03 19:55:34 +0100

    installation dotenv + TypeORM + mise écoute sur port 8080

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +6,17 @@ import { LoyaltycardsModule } from './loyaltycards/loyaltycards.module';
 import { CustomersModule } from './customers/customers.module';
 import { AnimationsrequestedModule } from './animationsrequested/animationsrequested.module';
 import { LocationsModule } from './locations/locations.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 @Module({
-  imports: [LoyaltycardsModule, CustomersModule, AnimationsrequestedModule, LocationsModule],
+  imports: [
+    LoyaltycardsModule,
+    CustomersModule,
+    AnimationsrequestedModule,
+    LocationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(8080);
 }
+
 bootstrap();
