4e02d7dcac4b3fe3d36bf333bb786a4c01fe7a61
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-09 13:36:17 +0100

    implémentation ClassValidator sur Table Loyalty_card - ok

--- src/loyaltycards/dto/create-loyaltycard.dto.ts
+++ src/loyaltycards/dto/create-loyaltycard.dto.ts
@@ -1,4 +1,26 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  MaxLength,
+} from '@nestjs/class-validator';
+
 export class CreateLoyaltycardDto {
+  //------------ CARD_NUMBER - Formattage par le biais de class-validator---------------
+  @MaxLength(5, {
+    message: 'Votre numéro de téléphone doit comporter cinq chiffres.',
+  })
+  @IsNumber()
+  @IsPositive({
+    message:
+      'Votre numéro de carte de fidélité ne doit comporter que cinq chiffres',
+  })
   card_number: number;
+
+  //------------ NUMBER_POINTS - Formattage par le biais de class-validator---------------
+  @IsNumber()
+  @IsPositive({
+    message: 'Votre nombre de points doit être strictement positif',
+  })
   number_of_points: number;
 }
--- src/loyaltycards/dto/update-loyaltycard.dto.ts
+++ src/loyaltycards/dto/update-loyaltycard.dto.ts
@@ -1,7 +1,23 @@
+import { IsNumber, IsPositive, MaxLength } from '@nestjs/class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLoyaltycardDto } from './create-loyaltycard.dto';
 
 export class UpdateLoyaltycardDto extends PartialType(CreateLoyaltycardDto) {
+  //------------ CARD_NUMBER - Formattage par le biais de class-validator---------------
+  @MaxLength(5, {
+    message: 'Votre numéro de téléphone doit comporter cinq chiffres.',
+  })
+  @IsNumber()
+  @IsPositive({
+    message:
+      'Votre numéro de carte de fidélité ne doit comporter que cinq chiffres',
+  })
   card_number: number;
+
+  //------------ NUMBER_POINTS - Formattage par le biais de class-validator---------------
+  @IsNumber()
+  @IsPositive({
+    message: 'Votre nombre de points doit être strictement positif',
+  })
   number_of_points: number;
 }
