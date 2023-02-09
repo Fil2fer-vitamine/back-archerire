c5971d472af1f746eed10716c122e1df9c98f88c
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-09 15:48:37 +0100

    Service Location: Ok

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(
+    @InjectRepository(Location)
+    private locationrepository: Repository<Location>,
+  ) {}
+
+  async create(createLocationDto: CreateLocationDto) {
+    return await this.locationrepository.save(createLocationDto);
   }
 
-  findAll() {
-    return `This action returns all locations`;
+  async findAll(): Promise<Location[]> {
+    return await this.locationrepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} location`;
+  async findOne(idValue: number): Promise<Location> {
+    const locationfound = await this.locationrepository.findOneBy({
+      id: idValue,
+    });
+    if (!locationfound) {
+      throw new NotFoundException(
+        `Désolé, nous n'avons pas trouvé d'endroit avec l'id ${idValue}.`,
+      );
+    }
+    return locationfound;
   }
 
-  update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
+  async update(
+    id: number,
+    updateLocationDto: UpdateLocationDto,
+  ): Promise<Location> {
+    const upLocation = await this.findOne(id);
+    upLocation.village_name = updateLocationDto.village_name;
+    return await this.locationrepository.save(upLocation);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} location`;
+  async remove(id: number): Promise<string> {
+    const Result = await this.locationrepository.delete({ id });
+    if (Result.affected === 0) {
+      throw new NotFoundException(
+        `Suppression impossible, car il n'y a pas d'endroit avec l'id ${id}.`,
+      );
+    }
+    return `Bravo: La catégorie avec l'id ${id} a bien été supprimée...`;
   }
 }
